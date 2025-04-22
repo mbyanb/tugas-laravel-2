3d378c898ac64840c4f443ea443f9adf1d4a93eb
Muhammad Byan <muhbyan03@gmail.com>
2025-04-22 21:02:06 +0700

    init

--- projek_laravel/database/migrations/2025_04_22_064622_create_products_table.php
+++ projek_laravel/database/migrations/2025_04_22_064622_create_products_table.php
@@ -13,9 +13,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->integer('stock');
-            $table->decimal('price', 10, 2);
             $table->timestamps();
         });
     }
